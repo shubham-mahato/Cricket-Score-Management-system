e620ee4e6aad19abb7714e65b52dae932b04b64f
Aditya Singh <imh10028.21@bitmesra.ac.in>
2022-06-20 16:27:20 +0530

    init repo

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\t\t\t\t\t\Project in C\t\t\t\t\n");
+    printf("\t\t=======================Cricket Score Management System==============================\t\t\n");
+    printf("\tIn This Project,We will manage whole Score System including runs,overs,wickets,Batsman details and Bowler details\n");
+    //printf("")
+
+    return 0;
+}
