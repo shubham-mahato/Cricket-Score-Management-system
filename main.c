faf8f30194125d196e812ac6c3e76d632118d9ae
Aditya Singh <imh10028.21@bitmesra.ac.in>
2022-06-20 17:01:06 +0530

    added main

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    printf("\t\t\t\t\t\Project in C\t\t\t\t\n");
+    printf("\t\t\t\t\tProject in C\t\t\t\t\n");
     printf("\t\t=======================Cricket Score Management System==============================\t\t\n");
     printf("\tIn This Project,We will manage whole Score System including runs,overs,wickets,Batsman details and Bowler details\n");
-    printf("Misra Ji is Best");
+
 
     return 0;
 }
