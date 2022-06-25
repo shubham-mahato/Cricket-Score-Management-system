5011141fd68b4050997c49d75bd787f26812dc6c
devseek <manojsingh20029835@gmail.com>
2022-06-25 23:19:28 +0530

    first batsman commit

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct batsman
+{
+    char name[11];
+    // name here represent name of batsman   
+    int sc;
+    // sc represent total score scored by batsman
+    int bp;
+    // bp represent balls played
+    float st;
+    // st represent strike rate 
+    
+};
+
+
 int main()
 {
     printf("\t\t\t\t\tProject in C\t\t\t\t\n");
@@ -8,5 +22,28 @@ int main()
     printf("\tIn This Project,We will manage whole Score System including runs,overs,wickets,Batsman details and Bowler details\n");
 
 
-    return 0;
+struct batsman details[11];
+
+for (int i = 0; i < 12; i++)
+{
+    printf("enter the detail of player ");
+
+    scanf("%s",details[i].name);
+
+    scanf("%d",&details[i].sc);
+
+    scanf("%d",&details[i].bp);
+
+    scanf("%f",&details[i].st);
+
+    if (details[i].bp==0)
+    {
+        break ;
+    }
+    
+
+}
+
+
+ return 0;
 }
