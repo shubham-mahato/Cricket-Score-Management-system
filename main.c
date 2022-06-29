f9a57fbd0d6e1f657961299a363f15bdca661fa1
devseek <manojsingh20029835@gmail.com>
2022-06-29 14:32:17 +0530

    automated system first commit

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
-<<<<<<< HEAD
 struct batsman
 {
-    char name[11];
+    char name[100];
     // name here represent name of batsman   
-    int sc;
+    char sc[50][200];
     // sc represent total score scored by batsman
     int bp;
     // bp represent balls played
@@ -16,97 +16,24 @@ struct batsman
 };
 
 
-=======
-struct Batsman
-{
-    char name[20];
-    int run,over,ones,twos,threes,four,six,balls_played;
 
-};
-struct Bowler
-{
-    char name[20];
-    int total_runs_given,overs,wickets;
-};
+int main(){
 
->>>>>>> ad1c5c07d3c194113ad03ddb472e719b63d19cb9
-int main()
-{
-    printf("\t\t\t\t\tProject in C\t\t\t\t\n");
-    printf("\t\t=======================Cricket Score Management System==============================\t\t\n");
-    printf("\tIn This Project,We will manage whole Score System including runs,overs,wickets,Batsman details and Bowler details\n");
+    char b[3] = "B";//bold
+    char co[3] = "CO";//catchout
+    char RO[3] = "RO";//runout
+    char sto[4] = "STO";//stumpout
+    char nb[3]  = "NB";//no ball
+    char db[3]  = "DB";// death ball
+    char wd[3]  =  "WD";// wide
 
-    int i,j,m,n;
-     printf("Enter the Batsman Details:\n");
-     printf("Enter the number of Batsman:\n");
-     scanf("%d",&m);
-     struct Batsman bt[12];
-     for ( i = 0; i <=m; i++)
-     {
-        printf("Enter the name of batsman %d\n",i+1);
-        scanf("%s",bt[i]);
-        printf("Enter the one made by batsman %d\n",i+1);
-        scanf("%d",&bt->ones);
-        printf("Enter the two made by batsman %d\n", i + 1);
-        scanf("%d", &bt->twos);
-        printf("Enter the three made by batsman %d\n", i + 1);
-        scanf("%d", &bt->threes);
-        printf("Enter the four made by batsman %d\n", i + 1);
-        scanf("%d", &bt->four);
-        printf("Enter the six made by batsman %d\n", i + 1);
-        scanf("%d", &bt->six);
-        printf("Enter the total numbers of ball played by batsman %d\n",i+1);
-        scanf("%d",&bt->balls_played);
-     }
+    int B , CO , ROU , STO , NB , DB , WD ;
 
-     printf("Enter the total number of Bowlers:\n");
-     scanf("%d",&n);
-     printf("Enter the Bowler details:\n");
-     struct Bowler bwl[10];
-     for ( j = 0; j<=n; i++)
-     {
-        printf("Enter the name of Bowler %d",j+1);
-        scanf("%s",bwl[i]);
-        printf("Enter the runs given by bowler %d",j+1);
-        scanf("%d",&bwl->total_runs_given);
-        printf("Enther the overs bowled by bowler %d",j+1);
-        scanf("%d",bwl->overs);
-        printf("Enter the total wickets taken by bowler %d",j+1);
-        scanf("%d",&bwl->wickets);
-     }
-
-     int choice,player_no;
-     while (choice<=4)
-     {
-
-        printf("Please enter your choice:\n 1)Batsman details:\n 2)Bowler details:\n 3)Match Details:\n 4)records:");
-        scanf("%d",&choice);
-        switch (choice)
-        {
-        case 1:
-           printf("Enter the batsman number to see his details:");
-           scanf("%d",&player_no);
-           player_no--;
-           printf("                          Player Detail\n");
-           printf("-----------------------------------------------------------------------------\n");
-           printf("  Batsman      runs      balls      four     six     threes     twos     ones");
-           printf("------------------------------------------------------------------------------\n");
-
-            break;
-
-        default:
-            break;
-        }
-     }
-
-
-
-
-
+    int end , i;
 
 struct batsman details[11];
 
-for (int i = 0; i < 12; i++)
+for (int i = 0; i < 11; i++)
 {
     printf("enter the detail of player ");
 
@@ -119,12 +46,121 @@ for (int i = 0; i < 12; i++)
     scanf("%f",&details[i].st);
 
     if (details[i].bp==0)
-    {
+    {   
+        end=i;
         break ;
     }
     
 
 }
+
+// struct scorecard
+// {
+//     int score ;
+
+// };
+
+    int n ;
+    printf("enter no. of overs \n");
+    scanf("%d",&n);
+
+   char arr[n][6];
+
+printf("enter run scored per ball");
+
+for (int i = 1; i <= (n); i++)
+{
+     printf("over no %d",i) ;
+
+    for (int j = 1; j <= 6; j++)
+    {
+        scanf("%s",arr[i][j]);
+    }
+    printf("\n");
+}
+
+struct runs
+{
+    int runs ;
+    int four ;
+    int six ;
+
+    struct batsman details[11];
+
+};
+int sum , sum6 , sum4 , sum1 ,sum2 , sum3 ;
+int six , four , one , two , three ;
+
+struct runs players[11];
+
+for (int i = 0; i < 11; i++)
+{
+    if (details[i].bp!=0)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            for (int k = 1; k <= 6; k++)
+            {
+               
+                //   B = strcmp(arr[j][k],"B");
+                //   CO= strcmp(arr[j][k],"CO");
+                //   RO = strcmp(arr[j][k],"RO");
+                //   STO = strcmp(arr[j][k],"STO");
+                //   NB  = strcmp(arr[j][k],"NB");
+                //   WD  = strcmp(arr[j][k],"WD");
+                //   DB  = strcmp(arr[j][k],"DB");
+                if (arr[j][k]!=b[k]||arr[j][k]!=co[k]||arr[j][k]!=RO[k]||arr[j][k]!=sto[k]||arr[j][k]!=nb[k]||arr[j][k]!=db[k]||arr[j][k]!=wd[k])
+                {
+                       if (arr[j][k]==6)
+                    {
+                        players[i].details[i].sc[j][k]=6 ;
+
+                     }
+
+                    else if (arr[j][k]==4)
+                     {
+                        players[i].details[i].sc[j][k]=4;
+                     }
+                     
+                    else if (arr[j][k]==2)
+                     {
+                        players[i].details[i].sc[j][k]=2;
+                     }
+                     
+                    else if (arr[j][k]==3)
+                     {
+                       players[i].details[i].sc[j][k]=3;
+                     }
+
+                     else if (arr[j][k]==1)
+                     {
+                        players[i].details[i].sc[j][k]=1;
+
+                        i++;
+                     }
+
+                     else{
+                        players[i].details[i].sc[j][k]=0;
+                     }
+                     
+                     
+                }
+                
+
+                    
+                  
+                   
+              
+            }
+            
+        }
+        
+    }
+    
+}
+
+
+
 
 
  return 0;
